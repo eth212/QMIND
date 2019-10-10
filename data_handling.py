b5721ef01aca97f7a3b163477a0f0822484c13ec
eth212 <ethanbonn21@gmail.com>
2019-10-10 16:49:18 -0400

    branch time

--- data_handling.py
+++ data_handling.py
@@ -1,4 +1,31 @@
 import numpy
 import pandas as pd
-use_cols = [i for i in range(27)]
-df = pd.read_csv("Data_0.csv",header=0,usecols=use_cols)
+
+df = pd.read_excel("Data_0.xlsx")
+data = pd.DataFrame(df)
+amount = 30000
+cars = []
+
+for i in range(len(data["SalesPrice"])):
+    if(data["Location"].iloc[i] == "DENVER, CO"):
+        cars.append(data["SalesPrice"].iloc[i])
+
+
+
+string = data["Notes"].iloc[2][:]
+
+for i in range(len(string)):
+    string[i]
+
+
+
+m = []
+for i in range(len(data)):
+    string = str(data["Notes"].iloc[i][0:4])
+    m.append(string)
+
+print(m)
+
+data["Midi_mini"] = m
+
+data["SalesPrice"].iloc[100:110]
