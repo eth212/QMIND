4a4183647c7bd9a153ea9d3175c9e398caea9ee3
eth212 <ethanbonn21@gmail.com>
2019-10-16 17:51:28 -0400

    cleaning up the tree

--- data_handling.py
+++ data_handling.py
@@ -30,28 +30,3 @@ print(m)
 data["Midi_mini"] = m
 
 data["SalesPrice"].iloc[100:110]
-=======
-use_cols = [i for i in range(27)]
-df = pd.read_excel("Data_0.xlsx",header=0,usecols=use_cols)
-
-#num_2012 = len([i for i in df['Year'] if i==2012])
-#print(num_2012)
-
-
-
-def separate_commas(dataframe,column):
-    column_data = dataframe[column]
-    column_better = []
-    for datapoint in column_data:
-        words = [""]
-        count = 0
-        for letter in datapoint:
-            if letter!=",":
-                words[count]+=letter
-            else:
-                words.append("")
-                count+=1
-        column_better.append(words)
-    dataframe[column]=column_better
-separate_commas(df,"Notes")
->>>>>>> d80b7bb7e39fad50487f6807932370ce4bb8abb6
