078e6017f21afe867a56c9fdcef186cd8f1ca614
eth212 <ethanbonn21@gmail.com>
2019-10-17 10:29:32 -0400

    fixing ignore

--- data_handling.py
+++ data_handling.py
@@ -1,3 +1,32 @@
-import numpy
-import pandas as pd 
+import numpy as np
+import pandas as pd
+<<<<<<< HEAD
 
+df = pd.read_excel("Data_0.xlsx")
+data = pd.DataFrame(df)
+amount = 30000
+cars = []
+
+for i in range(len(data["SalesPrice"])):
+    if(data["Location"].iloc[i] == "DENVER, CO"):
+        cars.append(data["SalesPrice"].iloc[i])
+
+
+
+string = data["Notes"].iloc[2][:]
+
+for i in range(len(string)):
+    string[i]
+
+
+
+m = []
+for i in range(len(data)):
+    string = str(data["Notes"].iloc[i][0:4])
+    m.append(string)
+
+print(m)
+
+data["Midi_mini"] = m
+
+data["SalesPrice"].iloc[100:110]
